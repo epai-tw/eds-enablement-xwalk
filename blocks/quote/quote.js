e4e811779acc118e5d9933ccde6aa0585c3123fb
Eason Pai <easonp.dev@gmail.com>
2025-09-14 17:20:21 +0800

    testing new branch

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
-
+  debugger
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
