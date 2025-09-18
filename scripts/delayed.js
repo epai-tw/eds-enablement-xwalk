2056506448b306518d1152659a12303d76adde8a
Eason Pai <easonp.dev@gmail.com>
2025-09-18 09:11:23 +0800

    integrating 3rd party libs

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,17 +1,9 @@
 // add delayed functionality here
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import $ from 'jquery';
+// import $ from 'jquery';
 
 // eslint-disable-next-line no-debugger
 debugger
 
-console.log('jQuery', $(
-  'body',
-));
-
-// 在這裡使用 jQuery
-$(document).ready(() => {
-  console.log('jQuery 已經成功載入！');
-  $('body').css('background-color', 'lightgray');
-});
+console.log('jQuery', $);
