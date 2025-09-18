ebdbbf25d0c63101f8393293cc9646f24700f09d
Eason Pai <easonp.dev@gmail.com>
2025-09-18 14:04:38 +0800

    adding new block

--- blocks/advantage-cards/advantage-cards.js
+++ blocks/advantage-cards/advantage-cards.js
@@ -1,117 +1,24 @@
-// export default function decorate(block) {
-//   const [quoteWrapper] = block.children;
-//   const blockquote = document.createElement('blockquote');
-//   blockquote.textContent = quoteWrapper.textContent.trim();
-//   quoteWrapper.replaceChildren(blockquote);
-// }
-
-
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const [wrapper] = block.children;
-  const carousel = document.createElement('carousel');
-  const container = `<div class="cmp-advantage-card">
-  <div class="cmp-advantage-card__image-wrapper">
-    <img
-      src="/content/dam/eds-enablement-xwalk/asus-cto-sites/advantage.jpg"
-      alt="Ultimate Gaming Experience"
-      class="cmp-advantage-card__image"
-    />
-    <video class="cmp-advantage-card__video" playsinline controls>
-      <source src="/content/dam/eds-enablement-xwalk/asus-cto-sites/videos/hero-banner-video-1.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-  </div>
-  <div class="cmp-advantage-card__overlay">
-    <div class="cmp-advantage-card__content">
-      <h3 class="cmp-advantage-card__title">Ultimate Gaming Experience</h3>
-      <p class="cmp-advantage-card__desc">
-        Our systems are engineered to enhance both performance and noise control, guaranteeing
-        stability and an unparalleled gaming experience.
-      </p>
-      <button class="cmp-advantage-card__btn btn">Watch now <img src="/content/dam/eds-enablement-xwalk/asus-cto-sites/icon-play-filled.svg" alt="play-icon" /></button>
-    </div>
-  </div>
-</div>`;
-  carousel.innerHTML = `<div class='container'>
-    <div class="carousel panelcontainer">
-      <div class="section-heading content-center">
-        <h2>Carousel 3D effect</h2>
-      </div>
-      <div
-        id="carousel-4e80c7e13a"
-        class="cmp-carousel"
-        role="group"
-        aria-live="polite"
-        aria-roledescription="carousel"
-        data-cmp-is="carousel"
-        data-cmp-delay="5000"
-        data-carousel-effect="creative"
-      >
-        <div class="cmp-carousel__content">
-          <div id="carousel-4e80c7e13a-item-30704e84cd-tabpanel" class="cmp-carousel__item cmp-carousel__item--active">
-            ${container.toString()}
-          </div>
-          <div id="carousel-4e80c7e13a-item-645fc67b90-tabpanel" class="cmp-carousel__item">
-            ${container.toString()}
-          </div>
-          <div id="carousel-4e80c7e13a-item-85d21a3332-tabpanel" class="cmp-carousel__item">
-            ${container.toString()}
-          </div>
-          <div id="carousel-4e80c7e13a-item-85d21a3334-tabpanel" class="cmp-carousel__item">
-            ${container.toString()}
-          </div>
-          <div id="carousel-4e80c7e13a-item-85d21a3335-tabpanel" class="cmp-carousel__item">
-            ${container.toString()}
-          </div>
-        </div>
-
-        <!-- Carousel actions - Previous/Next -->
-        <div class="cmp-carousel__actions">
-          <button class="cmp-carousel__action cmp-carousel__action--previous" type="button" aria-label="Previous">
-            <span class="icon icon--arrow-left"></span>
-          </button>
-          <button class="cmp-carousel__action cmp-carousel__action--next" type="button" aria-label="Next">
-            <span class="icon icon--arrow-right"></span>
-          </button>
-        </div>
-
-        <!-- Pagination indicatoors -->
-        <ol class="cmp-carousel__indicators" role="tablist" aria-label="Choose a slide to display"></ol>
-      </div>
-    </div>
-  </div>`;
-  wrapper.replaceChildren(carousel);
-//
-//   import $ from 'jquery';
-//
-//   // 在這裡使用 jQuery
-//   $(document).ready(() => {
-//     console.log('jQuery 已經成功載入！');
-//     $('body').css('background-color', 'lightgray');
-//   });
-//
-//   // /blocks/carousel/index.js
-//
-// // 定義 Swiper 初始化的函式
-//   function initSwiper(block) {
-//     // 確保 Swiper 函式已經載入
-//     if (window.Swiper) {
-//       // 使用 Swiper 函式來建立一個新的實例
-//       new Swiper(block.querySelector('.swiper'), {
-//         // 在這裡設定 Swiper 的參數
-//         loop: true,
-//         pagination: {
-//           el: '.swiper-pagination',
-//         },
-//         navigation: {
-//           nextEl: '.swiper-button-next',
-//           prevEl: '.swiper-button-prev',
-//         },
-//       });
-//     } else {
-//       // 如果 Swiper 還沒載入，可以延遲執行，直到載入完成
-//       setTimeout(() => initSwiper(block), 100);
-//     }
-//   }
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    moveInstrumentation(row, li);
+    while (row.firstElementChild) li.append(row.firstElementChild);
+    [...li.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.textContent = '';
+  block.append(ul);
 }
