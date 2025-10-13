082456859176e3b946294569638eeaae38eab579
Eason Pai <easonp.dev@gmail.com>
2025-10-13 09:54:27 +0800

    test using external header

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -110,7 +110,8 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  // const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  const navPath = 'https://main--eds-enablement-xwalk--epai-tw.aem.page/nav';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
