6521cead28864a645e1265ed4a518867fc58037a
Eason Pai <easonp.dev@gmail.com>
2025-10-14 13:40:28 +0800

    update components

--- blocks/advantage-cards/advantage-cards.js
+++ blocks/advantage-cards/advantage-cards.js
@@ -4,7 +4,20 @@ import { moveInstrumentation } from '../../scripts/scripts.js';
 export default function decorate(block) {
   console.log('>>>> advantage-cards');
   [...block.children].forEach((card) => {
+    let image;
+    let body;
+    let title;
+    let cta;
+
+    [...card.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        image = div;
+        image.className = 'cards-card-image';
+      } else{
+        body = div;
+        body.className = 'cards-card-body';
+      }
+    });
     console.log('>>>> advantage-cards >>', card);
-    
   });
 }
