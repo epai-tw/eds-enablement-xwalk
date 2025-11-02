ed24505878e36a3cc591884503a9cc1cb066219f
Eason Pai <epai@adobe.com>
2025-11-02 20:06:53 +0800

    updating comp

--- blocks/advantage-cards/advantage-cards.js
+++ blocks/advantage-cards/advantage-cards.js
@@ -26,7 +26,8 @@ function moveInstrumentation(from, to) {
 }
 
 export default function decorate(block) {
-  const mockupContainer = document.createRange().createContextualFragment(`<div class='container' data-aue-type="container" data-aue-behavior="component" data-aue-label="Advantage Cards" data-aue-filter="advantage-cards" data-block-name="advantage-cards">
+  const mockupContainer = document.createElement('template');
+  mockupContainer.innerHTML = `<div class='container' data-aue-type="container" data-aue-behavior="component" data-aue-label="Advantage Cards" data-aue-filter="advantage-cards" data-block-name="advantage-cards">
     <div class="carousel panelcontainer">
       <div class="section-heading content-center">
         <h2>Carousel 3D effect</h2>
@@ -57,7 +58,7 @@ export default function decorate(block) {
         <ol class="cmp-carousel__indicators" role="tablist" aria-label="Choose a slide to display"></ol>
       </div>
     </div>
-  </div>`);
+  </div>`;
 
   const cardNodes = [...block.children].map((card) => {
     const safeText = (el, fallback = '') => el?.textContent?.trim() ?? fallback;
@@ -69,8 +70,8 @@ export default function decorate(block) {
     const mediaHTML = card.querySelector('picture')?.innerHTML ?? '';
 
     if (headline === '') return;
-
-    const mockup = document.createRange().createContextualFragment(`
+    const tpl = document.createElement('template');
+    tpl.innerHTML = `
           <div class="cmp-carousel__item">
             <div class="cmp-advantage-card" 
             data-aue-type="container" data-aue-behavior="component" data-aue-label="Advantage Card" data-aue-filter="advantage-card" data-block-name="advantage-card">
@@ -93,12 +94,12 @@ export default function decorate(block) {
                 </div>
               </div>
             </div>
-          </div>`);
-    moveInstrumentation(card, mockup);
-
-    return mockup;
+          </div>`;
+    const element = tpl.content.firstElementChild;
+    moveInstrumentation(card, element);
+    return element;
   });
 
-  mockupContainer.querySelector('.cmp-carousel__content').append(...cardNodes);
+  mockupContainer.content.firstElementChild.querySelector('.cmp-carousel__content').append(...cardNodes);
   block.replaceWith(mockupContainer);
 }
