946d277e8c85ec859fb7bc83818bd82e4066f94b
Eason Pai <epai@adobe.com>
2025-11-02 20:30:46 +0800

    updating comp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,15 +18,17 @@ import {
  * @param {Element} to the element to copy attributes to
  */
 export function moveAttributes(from, to, attributes) {
-  console.log('moveAttributes>>>\n', from, "\n", to, "\n", attributes);
+  console.log('moveAttributes>>>\n');
   if (!attributes) {
     // eslint-disable-next-line no-param-reassign
     attributes = [...from.attributes].map(({ nodeName }) => nodeName);
   }
   attributes.forEach((attr) => {
     const value = from.getAttribute(attr);
+    console.log('>>> from >>>', ` ${attr}="${value}"`);
     if (value) {
       if (to instanceof DocumentFragment) {
+        console.log('>>> to >>>', ` ${to?.firstElementChild.innerHTML}`);
         to?.firstElementChild.setAttribute(attr, value);
       } else {
         to?.setAttribute(attr, value);
